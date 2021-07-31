1ef988f31ca9434634ff0084914300276b8cea1f
gabrieltherock <gabrieldarocha2013@hotmail.com>
2021-07-31 18:59:20 -0300

    conexão HTTP e HttpPolling com MySQL

--- src/main/java/br/com/gabriel/camel/RotaPedidos.java
+++ src/main/java/br/com/gabriel/camel/RotaPedidos.java
@@ -1,33 +1,35 @@
 package br.com.gabriel.camel;
 
 import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
 
 public class RotaPedidos {
 
-	public static void main(String[] args) throws Exception {
-		CamelContext context = new DefaultCamelContext();
+    public static void main(String[] args) throws Exception {
+        CamelContext context = new DefaultCamelContext();
 
-		context.addRoutes(new RouteBuilder() {
+        context.addRoutes(new RouteBuilder() {
+            @Override
+            public void configure() {
+                from("file:pedidos?delay=5s&noop=true")
+                        .setProperty("pedidoId", xpath("/pedido/id/text()"))
+                        .setProperty("clienteId", xpath("/pedido/pagamento/email-titular/text()"))
+                        .split()
+                        .xpath("/pedido/itens/item")
+                        .filter()
+                        .xpath("/item/formato[text()='EBOOK']")
+                        .setProperty("ebookId", xpath("/item/livro/codigo/text()"))
+                        .log("${id} \n ${body}")
+                        .marshal().xmljson()
+                        .setHeader(Exchange.HTTP_QUERY, simple("clienteId=${property.clienteId}&pedidoId=${property.pedidoId}&ebookId=${property.ebookId}"))
+                        .to("http4://localhost:8080/webservices/ebook/item");
+            }
+        });
 
-			@Override
-			public void configure() {
-				from("file:pedidos?delay=5s&noop=true")
-						.split()
-							.xpath("/pedido/itens/item")
-						.filter()
-							.xpath("/item/formato[text()='EBOOK']")
-							.log("${id}")
-						.marshal().xmljson()
-							.log("${body}")
-							.setHeader("CamelFileName", simple("${file:name.noext}.json"))
-						.to("file:saida");
-			}
-		});
-
-		context.start();
-		Thread.sleep(20000);
-		context.stop();
-	}	
+        context.start();
+        Thread.sleep(20000);
+        context.stop();
+    }
 }
