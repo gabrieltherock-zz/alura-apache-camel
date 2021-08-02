2ade134d03691506178d56959f34f6c196aab392
gabrieltherock <gabrieldarocha2013@hotmail.com>
2021-08-01 21:41:43 -0300

    sub-rotas, multicast e routeId

--- src/main/java/br/com/gabriel/camel/RotaPedidos.java
+++ src/main/java/br/com/gabriel/camel/RotaPedidos.java
@@ -13,18 +13,29 @@ public class RotaPedidos {
         context.addRoutes(new RouteBuilder() {
             @Override
             public void configure() {
-                from("file:pedidos?delay=5s&noop=true")
-                        .setProperty("pedidoId", xpath("/pedido/id/text()"))
-                        .setProperty("clienteId", xpath("/pedido/pagamento/email-titular/text()"))
-                        .split()
-                        .xpath("/pedido/itens/item")
-                        .filter()
-                        .xpath("/item/formato[text()='EBOOK']")
-                        .setProperty("ebookId", xpath("/item/livro/codigo/text()"))
-                        .log("${id} \n ${body}")
-                        .marshal().xmljson()
-                        .setHeader(Exchange.HTTP_QUERY, simple("clienteId=${property.clienteId}&pedidoId=${property.pedidoId}&ebookId=${property.ebookId}"))
-                        .to("http4://localhost:8080/webservices/ebook/item");
+                from("file:pedidos?delay=5s&noop=true").
+                        routeId("rota-pedidos").
+                        multicast().
+                        to("direct:soap").
+                        to("direct:http");
+
+                from("direct:soap").
+                        routeId("rota-soap").
+                        log("chamando servico soap ${body}").
+                        to("mock:soap");
+
+                from("direct:http").
+                        routeId("rota-http").
+                        setProperty("pedidoId", xpath("/pedido/id/text()")).
+                        setProperty("email", xpath("/pedido/pagamento/email-titular/text()")).
+                        split().
+                        xpath("/pedido/itens/item").
+                        filter().
+                        xpath("/item/formato[text()='EBOOK']").
+                        setProperty("ebookId", xpath("/item/livro/codigo/text()")).
+                        setHeader(Exchange.HTTP_QUERY,
+                                simple("clienteId=${property.email}&pedidoId=${property.pedidoId}&ebookId=${property.ebookId}")).
+                        to("http4://localhost:8080/webservices/ebook/item");
             }
         });
 
